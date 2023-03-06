b04e1035a6f397764d1e967ab9de3afaac0d784b
Trinh Van Son <sontrinh.tp@gmail.com>
2023-03-07 00:03:05 +0700

    add file

---
+++ app.js
@@ -0,0 +1,10 @@
+let r = prompt('Nhập bán kính của đường tròn:');
+let d = r * 2;
+let c = d * Math.PI;
+let s = r^2 * Math.PI;
+
+document.write('Đường kính đường tròn: ' + d);
+document.write('<br>');
+document.write('Chu vi đường tròn: ' + c);
+document.write('<br>');
+document.write('Diện tích đường tròn: ' + s);
